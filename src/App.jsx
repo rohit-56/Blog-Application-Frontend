fe9dc7b7fda2ddbf55432ab867fa92a895228321
Rohit <rohit.sharma2@hexaviewtech.com>
2024-03-14 13:15:32 +0530

    Create a common navbar for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './App.css'
 import { Route , Routes } from 'react-router-dom'
-import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Base from './components/Base'
 
 const App = () => {
   return (
-    <div className='container'>
-      <Routes>
-      <Route path='/' exact element = {<Home/>} />
+    
+    <Routes>
+      <Route path='/' exact element = {<Base/>} />
       <Route path='/blog/:id' element={<Blog/>} />
       </Routes>
+
+     
       
-    </div>
   )
 }
 
