85e7451e2181043e01337d273b2f3c0b85421f39
Rohit <rohit.sharma2@hexaviewtech.com>
2024-03-15 20:47:45 +0530

    create login and signup page

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,13 +1,12 @@
 import BlogNavbar from "./BlogNavbar";
 
-const Base = ({children})=>{
-    return (
-      <div className="container-fluid p-0 m-0">
-        <BlogNavbar />
-        {children}
-      </div>
-
-    );   
-}
+const Base = ({ children }) => {
+  return (
+    <div className="container-fluid p-0 m-0">
+      <BlogNavbar />
+      {children}
+    </div>
+  );
+};
 
 export default Base;
