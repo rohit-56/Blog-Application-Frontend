77af3e2eb8c5a9a593bbb9ab53b67f618b61c2a2
Rohit <rohit.sharma2@hexaviewtech.com>
2024-04-14 17:41:04 +0530

    create user profile UI

--- src/components/user/profileinfo.jsx
+++ src/components/user/profileinfo.jsx
@@ -1,10 +1,64 @@
 import React from "react";
 import Base from "../Base";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardText,
+  Container,
+  Label,
+  Row,
+  Col,
+} from "reactstrap";
 
 const ProfileInfo = () => {
   return (
     <Base>
-      <div>ProfileInfo</div>
+      <Container className="mt-2">
+        <Label tag={"h2"} className="text-center">
+          User Information
+        </Label>
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            borderRadius: "10px",
+          }}
+        >
+          <Card className="mt-2 shadow-lg p-3 mb-5 bg-white rounded">
+            <CardBody>
+              <Row>
+                <Col>
+                  <Container>
+                    <img alt="Sample" src="https://picsum.photos/300/200" />
+                  </Container>
+                </Col>
+                <Col>
+                  <Container>
+                    <CardTitle tag={"h3"}>Username</CardTitle>
+                    <CardSubtitle tag={"h6"}>rohitmou25@gmail.com</CardSubtitle>
+                    <CardText className="my-2" tag={"h5"}>
+                      About
+                    </CardText>
+                    <CardText
+                      style={{
+                        background: "#E5E4E2",
+                        height: "80px",
+                        width: "400px",
+                      }}
+                    >
+                      I am Developer
+                    </CardText>
+                    <Button color="primary">Edit Profile</Button>
+                  </Container>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Container>
+      </Container>
     </Base>
   );
 };
